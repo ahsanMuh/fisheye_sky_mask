47e3b5b6922021dc7a9e1b3f580ccbed0eb49769
ahsanYleg <ahsanmuhammadakram1996@gmail.com>
2022-03-30 15:46:40 +0500

    Added debugging in mask generation

--- mask_generator.py
+++ mask_generator.py
@@ -39,22 +39,27 @@ def img_to_maskpath(org):
     # using background removal to get buildings (not clear enough)
     img = np.array(org)
     img = rgb2gray(img)
+    print('converted the image into required format ...')
     background = rolling_ball(img)
     img = img - background
+    print('Got the background ...')
 
     #image processing
     bn_open = binary_opening(img)
     bn_open_inv = invert(bn_open)
     bn_open_inv_big = remove_small_objects(bn_open_inv, min_size=1000)
+    print('removed small objects ...')
 
     # getting contours
     bn_open_inv_big_lab = label(bn_open_inv_big)
     res = regionprops(bn_open_inv_big_lab)
+    print('got the contours ...')
 
     # find the middle point
     centre = (img.shape[0]/2, img.shape[1]/2)
     distances = [get_min_dis(cont, centre) for cont in res]
     
+
     # get the right index
     for i in range(len(distances) - 1):
         ind = np.where(distances == np.partition(distances, i+1)[i])[0][0]
@@ -63,10 +68,12 @@ def img_to_maskpath(org):
         if area_percent > MIN_SKY_SIZE:
             break
 
+    print('Found the required contor ...')
     # now creating mask
     mask = np.zeros(img.shape)
     bbox = res[ind].bbox
     mask[bbox[0] : bbox[2], bbox[1] : bbox[3]] = res[ind].image
+    print('about to save results ...')
 
     # in a landscape image, rotate the mask
     if org.width > org.height:
